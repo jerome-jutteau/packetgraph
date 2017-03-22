d8794311f6e3736c93ef7554ee1f0671ee0ecee8
Jerome Jutteau <jerome.jutteau@outscale.com>
2017-03-22 14:04:38 +0100

    examples: restore dperf
    Signed-off-by: Jerome Jutteau <jerome.jutteau@outscale.com>

--- examples/dperf/dperf.c
+++ examples/dperf/dperf.c
@@ -15,6 +15,8 @@
  * along with Packetgraph.  If not, see <http://www.gnu.org/licenses/>.
  */
 
+#include <stdlib.h>
+#include <stdio.h>
 #include <unistd.h>
 #include <signal.h>
 #include <glib.h>
@@ -22,21 +24,12 @@
 #include <arpa/inet.h>
 #include <sys/time.h>
 #include <time.h>
-
-#include <rte_config.h>
-#include <rte_ether.h>
-#include <rte_ethdev.h>
-#include <rte_ip.h>
-
+#include <string.h>
+#include <netinet/ether.h>
+#include <linux/if_ether.h>
+#include <linux/ip.h>
+#include <arpa/inet.h>
 #include <packetgraph/packetgraph.h>
-#include <packetgraph/nic.h>
-#include <packetgraph/vhost.h>
-#include <packetgraph/print.h>
-#include <packetgraph/brick.h>
-#include <packetgraph/utils/mempool.h>
-#include <packetgraph/utils/bitmask.h>
-#include <packetgraph/utils/mac.h>
-#include <packetgraph/packets.h>
 
 #define CHECK_ERROR(error) do {			\
 		if (error)			\
@@ -44,99 +37,159 @@
 		g_assert(!error);		\
 	} while (0)
 
-static int start_loop(int32_t ip_src, int32_t ip_dst,
-		      struct ether_addr src_mac,
-		      struct ether_addr dst_mac,
-		      int mtu)
+#define DATA_LEN 1000
+
+struct packet {
+	struct ethhdr eth;
+	struct iphdr ip;
+	char payload[DATA_LEN];
+} __attribute__((__packed__));
+
+#define PKT_LEN sizeof(struct packet)
+
+static void tx_callback(struct pg_brick *brick,
+			struct pg_rxtx_packet *tx_burst,
+			uint16_t *tx_burst_len,
+			void *private_data)
 {
-	int ret = -1;
-	uint64_t mask = ~0LL;
-	struct rte_mbuf **pkts = pg_packets_create(mask);
-	struct pg_error *error = NULL;
-	struct pg_brick *nic = pg_nic_new_by_id("nic", 1, 1,
-						WEST_SIDE, 0, &error);
-	CHECK_ERROR(error);
-	char	pkt[1500];
+	static bool copied = false;
 
-	memset(pkt, ~0, 1500);
-	pkt[1499] = '\0';
-
-	pg_packets_append_ether(pkts, mask, &src_mac,
-				&dst_mac, ETHER_TYPE_IPv4);
-	pg_packets_append_ipv4(pkts, mask, ip_src, ip_dst, mtu, 253);
-	pkt[mtu - sizeof(struct ipv4_hdr) - sizeof(struct ether_hdr)] = '\0';
-	pg_packets_append_str(pkts, mask, pkt);
-
-	while (1) {
-		pg_brick_burst_to_west(nic, 0, pkts, 64, mask, &error);
-		CHECK_ERROR(error);
-		usleep(1);
+	if (!copied) {
+		memcpy(tx_burst[0].data, private_data, PKT_LEN);
+		*tx_burst[0].len = PKT_LEN;
+		copied = true;
 	}
-	return ret;
+	*tx_burst_len = 1;
+}
+
+/* dpdk steals -h and --help */
+static void check_help(int argc, char **argv)
+{
+	bool print = false;
+	for (int a = 1; a < argc; a++) {
+		if (g_str_equal(argv[a], "-h") ||
+		    g_str_equal(argv[a], "--help")) {
+			print = true;
+			break;
+		}
+	}
+	if (print) {
+		printf("%s usage: [EAL_OPTIONS] -- [DPERF_OPTIONS]\n", argv[0]);
+		printf("\n");
+		printf("DPERF_OPTIONS:\n");
+		printf("\n");
+		printf("  --smac MAC          packet src mac address\n");
+		printf("  --dmac MAC          packet dst mac address\n");
+		printf("  --sip IP            packet src ip address\n");
+		printf("  --dip IP            packet dst ip address\n");
+		printf("  --help, -h          show this help\n");
+		printf("\n");
+	}
 }
 
 int main(int argc, char **argv)
 {
 	struct pg_error *error = NULL;
 	int ret = -1;
+	struct pg_brick *nic, *burster;
+	struct pg_graph *graph;
+	static struct packet pkt;
 
-	struct ether_addr src_mac;
-	struct ether_addr dst_mac;
-	int32_t src_ip = 0;
-	int32_t dst_ip = 0;
-	int nbVal = 0;
-
+	check_help(argc, argv);
 	ret = pg_start(argc, argv, &error);
 	g_assert(ret != -1);
 	CHECK_ERROR(error);
-	argc -= ret;
-	argv += ret;
-	while (argc > 1) {
-		if (g_str_equal(argv[1], "-sip")) {
-			if (argc < 2)
+
+	pkt.eth.h_proto = htons(ETH_P_IP);
+	pkt.ip.ihl = 5;
+	pkt.ip.version = 4;
+	pkt.ip.protocol = IPPROTO_UDP;
+	pkt.ip.tot_len = htons(DATA_LEN);
+
+	argc -= ret + 1;
+	argv += ret + 1;
+	ret = 1;
+	while (argc > 0) {
+		if (g_str_equal(argv[0], "--sip")) {
+			if (argc < 2) {
+				fprintf(stderr, "--sip needs argument\n");
 				goto exit;
-			src_ip = inet_addr(argv[2]);
-			if (src_ip == -1)
+			}
+			if (inet_aton(argv[1],
+				      (struct in_addr *) &pkt.ip.saddr) < 0) {
+				fprintf(stderr, "ip parsing for --sip failed\n");
 				goto exit;
+			}
 			--argc;
 			++argv;
-			++nbVal;
-		} else if (g_str_equal(argv[1], "-dip")) {
-			if (argc < 2)
+		} else if (g_str_equal(argv[0], "--dip")) {
+			if (argc < 2) {
+				fprintf(stderr, "--dip needs argument\n");
 				goto exit;
-			dst_ip = inet_addr(argv[2]);
-			if (dst_ip == -1)
+			}
+			if (inet_aton(argv[1],
+				      (struct in_addr *) &pkt.ip.daddr) < 0) {
+				fprintf(stderr, "ip parsing for --dip failed\n");
 				goto exit;
+			}
 			--argc;
 			++argv;
-			++nbVal;
-		} else if (g_str_equal(argv[1], "-dmac")) {
-			if (argc < 2)
+		} else if (g_str_equal(argv[0], "--dmac")) {
+			if (argc < 2) {
+				fprintf(stderr, "--dmac needs argument\n");
 				goto exit;
-			pg_scan_ether_addr(&dst_mac, argv[2]);
-			if (!is_valid_assigned_ether_addr(&dst_mac))
+			}
+			struct ether_addr *dst_mac = ether_aton(argv[1]);
+
+			if (!dst_mac) {
+				fprintf(stderr, "parsing for --dmac failed\n");
 				goto exit;
+			}
+			memcpy(&pkt.eth.h_dest, dst_mac, ETH_ALEN);
 			--argc;
 			++argv;
-			++nbVal;
-		} else if (g_str_equal(argv[1], "-smac")) {
-			if (argc < 2)
+		} else if (g_str_equal(argv[0], "--smac")) {
+			if (argc < 2) {
+				fprintf(stderr, "--smac needs argument\n");
 				goto exit;
-			pg_scan_ether_addr(&src_mac, argv[2]);
-			if (!is_valid_assigned_ether_addr(&src_mac))
+			}
+			struct ether_addr *src_mac = ether_aton(argv[1]);
+
+			if (!src_mac) {
+				fprintf(stderr, "parsing for --smac failed\n");
 				goto exit;
+			}
+			memcpy(&pkt.eth.h_source, src_mac, ETH_ALEN);
 			--argc;
 			++argv;
-			++nbVal;
+		} else {
+			fprintf(stderr, "unkown argument %s\n", argv[0]);
+			goto exit;
 		}
 		--argc;
 		++argv;
 	}
 
-	if (nbVal != 4)
+
+	if (!pg_nic_port_count()) {
+		fprintf(stderr, "no dpdk port, try using --vdev=eth_pcap0,iface=eth0\n");
 		goto exit;
-	/* accounting program name */
-	ret = start_loop(src_ip, dst_ip, src_mac, dst_mac, 1500);
+	}
+
+	nic = pg_nic_new_by_id("port 0", 0, &error);
+	CHECK_ERROR(error);
+	burster = pg_rxtx_new("tx", NULL, &tx_callback, (void *) &pkt);
+	pg_brick_link(nic, burster, &error);
+	CHECK_ERROR(error);
+	graph = pg_graph_new("dperf", nic, &error);
+	CHECK_ERROR(error);
+
+	printf("let's burst packets !\n");
+	while (42)
+		if (pg_graph_poll(graph, &error) < 0)
+			break;
+	CHECK_ERROR(error);
+	ret = 0;
 exit:
 	pg_stop();
 	return ret;
